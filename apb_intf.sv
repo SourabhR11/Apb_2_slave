27798aece9e59050c21627e058483108afb018da
Sourabh <147809263+SourabhR11@users.noreply.github.com>
2025-04-28 16:49:33 +0530

    Update apb_intf.sv

--- apb_intf.sv
+++ apb_intf.sv
@@ -1,21 +1,21 @@
 
-interface apb_intf(input bit pclk, input bit presetn);
+interface apb_intf(input bit PCLK, input bit PRESETn);
 
   
 
   //define signals as logic
   logic transfer;
-  logic read_write;
+  logic READ_WRITE;
   logic [`AW-1:0] apb_write_paddr;
   logic [`AW-1:0] apb_read_paddr;
   logic [`DW-1:0] apb_write_data;
   logic [`DW-1:0] apb_read_data_out;
 
   //clocking block for driver
-  clocking drv_cb @(posedge pclk);
+  clocking drv_cb @(posedge PCLK);
     default input #0 output #0;
     output transfer;
-    output read_write;
+    output READ_WRITE;
     output apb_write_paddr;
     output apb_read_paddr;
     output apb_write_data;
@@ -23,10 +23,10 @@ interface apb_intf(input bit pclk, input bit presetn);
   endclocking
 
   //clocking block for monitor
-  clocking mon_cb @(posedge pclk);
+  clocking mon_cb @(posedge PCLK);
     default input #0 output #0;
     input transfer;
-    input read_write;
+    input READ_WRITE;
     input apb_write_paddr;
     input apb_read_paddr;
     input apb_write_data;  
