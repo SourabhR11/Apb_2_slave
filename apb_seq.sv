8a93f582ebc4c7fc0152917bcf2fc21b97e94149
Sourabh <147809263+SourabhR11@users.noreply.github.com>
2025-05-14 22:49:54 +0530

    Update apb_seq.sv

--- apb_seq.sv
+++ apb_seq.sv
@@ -27,7 +27,7 @@ class apb_seq extends uvm_sequence #(apb_seq_item);
 endclass
 
 
-////////// apb_write sequence ////////////////////////
+////////// apb_write_slave1_sequence ////////////////////////
 class apb_write_slave1 extends apb_seq;
 
   //factory registration
@@ -52,9 +52,36 @@ class apb_write_slave1 extends apb_seq;
     end
   endtask
 endclass
+
+////////// apb_write_slave2_sequence ////////////////////////
+class apb_write_slave2 extends apb_seq;
+
+  //factory registration
+  `uvm_object_utils(apb_write_slave2)
+  
+  //class constructor
+  function new (string name = "apb_write_slave2" );
+    super.new(name);
+  endfunction
+
+  apb_seq_item item;
+
+  virtual task body();
+   repeat(5)begin
+    item = apb_seq_item::type_id::create("item");
+      `uvm_do_with(item,{
+                        transfer == 1'b1;
+                        READ_WRITE == 1'b0;
+                        apb_write_paddr[8] == 1'b1;
+                        })
+      `uvm_send(item);
+    end
+  endtask
+endclass
+                       
                        
  
-///////////// apb_read_sequence ////////////////////////////
+///////////// apb_read_slave1_sequence ////////////////////////////
 class apb_read_slave1 extends apb_seq;
 
   //factory registration
@@ -79,7 +106,32 @@ class apb_read_slave1 extends apb_seq;
   endtask
 endclass
 
-/////////// apb_write_read_sequence //////////////////////
+///////////// apb_read_slave2_sequence ////////////////////////////
+class apb_read_slave2 extends apb_seq;
+
+  //factory registration
+  `uvm_object_utils(apb_read_slave2)
+  
+  //class constructor
+  function new (string name = "apb_read_slave2" );
+    super.new(name);
+  endfunction
+
+  apb_seq_item item;
+  virtual task body();
+    repeat(10) begin
+    item = apb_seq_item::type_id::create("item");
+    `uvm_do_with(item,{
+                       transfer == 1'b1;
+                       READ_WRITE == 1'b1;
+                       apb_read_paddr[8] == 1'b1;
+                      })
+   `uvm_send(item);
+   end
+  endtask
+endclass
+
+/////////// apb_write_read_slave1_sequence //////////////////////
 
 class apb_write_read_slave1 extends apb_seq;
  
@@ -96,6 +148,33 @@ class apb_write_read_slave1 extends apb_seq;
 virtual task body();
     repeat(5)begin
      `uvm_do_with(item, {item.transfer == 1; item.READ_WRITE == 0;item.apb_write_paddr[8] == 0;})
+     `uvm_send(item);
+     addr = item.apb_write_paddr;
+ 
+    `uvm_do_with(item, {item.transfer == 1; item.READ_WRITE == 1; item.apb_read_paddr == addr;})
+
+     `uvm_send(item);
+    end
+  endtask
+endclass
+
+/////////// apb_write_read_slave2_sequence //////////////////////
+
+class apb_write_read_slave2 extends apb_seq;
+ 
+  `uvm_object_utils(apb_write_read_slave2)
+ 
+  function new(string name = "apb_write_read_slave2");
+    super.new(name);
+  endfunction
+ 
+   apb_seq_item item;
+
+   bit [8:0]addr;
+
+virtual task body();
+    repeat(5)begin
+      `uvm_do_with(item, {item.transfer == 1; item.READ_WRITE == 0;item.apb_write_paddr[8] == 1;})
      `uvm_send(item);
      addr = item.apb_write_paddr;
  
