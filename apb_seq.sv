8e517e692d7d5dd44a7160a342b89c912da3488d
SourabhR11 <147809263+SourabhR11@users.noreply.github.com>
2025-05-19 10:18:06 +0530

    updated

--- apb_seq.sv
+++ apb_seq.sv
@@ -214,6 +214,35 @@ virtual task body();
   endtask
 endclass
 
+ /////////// apb_10write_1read_slave2_sequence //////////////////////
+
+class apb_10write_1read_slave2 extends apb_seq;
  
+  `uvm_object_utils(apb_10write_1read_slave2)
+ 
+  function new(string name = "apb_10write_1read_slave");
+    super.new(name);
+  endfunction
+ 
+   apb_seq_item item;
+
+  bit [8:0] last_addr;
+
+virtual task body();
+   // repeat(5)begin
+   for (int i = 0; i < 10; i++) begin
+     `uvm_do_with(item, {item.transfer == 1; item.READ_WRITE == 0; item.apb_write_paddr[8] == 1;})
+      `uvm_send(item);
+     
+       last_addr = item.apb_write_paddr;
+    end 
+     `uvm_do_with(item, {item.transfer == 1; item.READ_WRITE == 1; item.apb_read_paddr == last_addr;})
+     `uvm_send(item);
+    
+   // end
+  endtask
+endclass
+
+
 
 
